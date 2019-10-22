7ddb5b8148e93d408d9a3425334a785b68829986
Chris Beyer <chris.beyer@staticly.com>
2019-10-22 14:00:44 +0800

    added change 1

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <h3>Git Branch: sta12347-development</h3>
-                <p>What a nice branch!</p>
+                <p>What a nice branch! Here is change 1.</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
